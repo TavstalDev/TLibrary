50243866435119cf98f2b79d7512b06d05caa5ba
Tavstal <tavstal@gmail.com>
2023-06-02 13:17:07 +0200

    Updated db converter

--- TLibrary/Helpers/DatabaseHelper.cs
+++ TLibrary/Helpers/DatabaseHelper.cs
@@ -101,11 +101,41 @@ namespace Tavstal.TLibrary.Helpers
             }
         }
 
-        public static void CreateTable<T>(this MySqlConnection connection, T schemaObj)
+        public static T ConvertToObject<T>(this MySqlDataReader reader)
+        {
+            if (!reader.HasRows)
+                return default(T); // Return default value for the type T
+
+            T obj = Activator.CreateInstance<T>();
+            var properties = typeof(T).GetProperties();
+
+            foreach (var property in properties)
+            {
+                if (reader[property.Name] != DBNull.Value)
+                {
+                    object value = reader[property.Name];
+                    property.SetValue(obj, value);
+                }
+            }
+
+            var fields = typeof(T).GetFields();
+            foreach (var field in fields)
+            {
+                if (reader[field.Name] != DBNull.Value)
+                {
+                    object value = reader[field.Name];
+                    field.SetValue(obj, value);
+                }
+            }
+
+            return obj;
+        }
+
+        public static void CreateTable<T>(this MySqlConnection connection)
         {
             try
             {
-                var schemaType = schemaObj.GetType();
+                var schemaType = typeof(T);
                 var attribute = schemaType.GetCustomAttribute<SqlNameAttribute>();
                 if (attribute == null)
                     throw new ArgumentNullException("The given schemaObj does not have SqlNameAttribute.");
@@ -143,6 +173,28 @@ namespace Tavstal.TLibrary.Helpers
                     schemaParams += $"{propName} {ConvertToSqlDataType(prop.PropertyType)}{nullableString},";
                 }
 
+                foreach (var prop in schemaType.GetFields())
+                {
+                    var propAttribute = prop.GetCustomAttribute<SqlNameAttribute>();
+                    string propName = propAttribute == null ? prop.Name : propAttribute.Name;
+
+                    string nullableString = string.Empty;
+                    if (prop.GetCustomAttribute<SqlNonNullableAttribute>() != null)
+                        nullableString = $" NOT NULL";
+
+                    if (prop.GetCustomAttribute<SqlPrimaryKeyAttribute>() != null)
+                        keyParams += $"PRIMARY KEY({propName}),";
+
+                    if (prop.GetCustomAttribute<SqlUniqueKeyAttribute>() != null)
+                        keyParams += $"UNIQUE ({propName}),";
+
+                    var foreignKey = prop.GetCustomAttribute<SqlForeignKeyAttribute>();
+                    if (foreignKey != null)
+                        keyParams += $"FOREIGN KEY ({propName}) REFERENCES {foreignKey.TableName}({foreignKey.TableColumn}),";
+
+                    schemaParams += $"{propName} {ConvertToSqlDataType(prop.FieldType)}{nullableString},";
+                }
+
                 command.CommandText = $"CREATE TABLE {attribute.Name} ({schemaParams}{keyParams})";
                 command.CommandText = command.CommandText.Remove(command.CommandText.LastIndexOf(','), 1);
                 command.ExecuteNonQuery();
@@ -154,24 +206,42 @@ namespace Tavstal.TLibrary.Helpers
             }
         }
 
-        public static void AddTableRow<T>(this MySqlConnection connection, T schemaObj)
+        public static void AddTableRow<T>(this MySqlConnection connection, T value)
         {
             try
             {
-                var schemaType = schemaObj.GetType();
-                var attribute = schemaType.GetCustomAttribute<SqlNameAttribute>();
-                if (attribute == null)
+                var schemaType = typeof(T);
+                var tableAttribute = schemaType.GetCustomAttribute<SqlNameAttribute>();
+                if (tableAttribute == null)
                     throw new ArgumentNullException("The given schemaObj does not have SqlNameAttribute.");
 
                 MySqlCommand MySQLCommand = connection.CreateCommand();
-                connection.Open();
 
-                MySQLCommand.CommandText = $"SELECT * FROM {attribute.Name} WHERE ";
-                object data = MySQLCommand.ExecuteScalar();
+                string paramString = string.Empty;
+                string keyString = string.Empty;
 
-                if (data == null)
-                    return;
-                MySQLCommand.CommandText = $"INSERT INTO {attribute.Name} () VALUES ()";
+                foreach (var prop in schemaType.GetProperties())
+                {
+                    var propAttribute = prop.GetCustomAttribute<SqlNameAttribute>();
+                    string propName = propAttribute == null ? prop.Name : propAttribute.Name;
+
+                    keyString += $"{propName},";
+                    paramString += $"`{prop.GetValue(value)}`,";
+                }
+
+                foreach (var prop in schemaType.GetFields())
+                {
+                    var propAttribute = prop.GetCustomAttribute<SqlNameAttribute>();
+                    string propName = propAttribute == null ? prop.Name : propAttribute.Name;
+
+                    keyString += $"{propName},";
+                    paramString += $"`{prop.GetValue(value)}`,";
+                }
+
+                paramString = paramString.Remove(paramString.LastIndexOf(','), 1);
+                keyString = keyString.Remove(keyString.LastIndexOf(','), 1);
+
+                MySQLCommand.CommandText = $"INSERT INTO {tableAttribute.Name} ({keyString}) VALUES ({paramString})";
                 MySQLCommand.ExecuteNonQuery();
             }
             catch (Exception ex)
@@ -181,27 +251,84 @@ namespace Tavstal.TLibrary.Helpers
             }
         }
 
-        public static void FillParametersFromObject<T>(this MySqlCommand command, T obj)
+        public static void UpdateTableRow<T>(this MySqlConnection connection, T newvalue)
         {
-            foreach (var property in obj.GetType().GetProperties())
+            try
             {
-                Console.WriteLine($"FILL PROPERTY DEBUG: @{property.Name.Normalize()}, Value: {property.GetValue(obj)}");
-                command.Parameters.AddWithValue($"@{property.Name.Normalize()}", property.GetValue(obj));
-            }
+                var schemaType = typeof(T);
+                var tableAttribute = schemaType.GetCustomAttribute<SqlNameAttribute>();
+                if (tableAttribute == null)
+                    throw new ArgumentNullException("The given schemaObj does not have SqlNameAttribute.");
 
-            foreach (var field in obj.GetType().GetFields())
+                MySqlCommand MySQLCommand = connection.CreateCommand();
+
+                string paramString = string.Empty;
+                string keyString = string.Empty;
+
+                foreach (var prop in schemaType.GetProperties())
+                {
+                    var propAttribute = prop.GetCustomAttribute<SqlNameAttribute>();
+                    string propName = propAttribute == null ? prop.Name : propAttribute.Name;
+
+                    keyString += $"{propName},";
+                    paramString += $"`{prop.GetValue(newvalue)}`,";
+                }
+
+                foreach (var prop in schemaType.GetFields())
+                {
+                    var propAttribute = prop.GetCustomAttribute<SqlNameAttribute>();
+                    string propName = propAttribute == null ? prop.Name : propAttribute.Name;
+
+                    keyString += $"{propName},";
+                    paramString += $"`{prop.GetValue(newvalue)}`,";
+                }
+
+                paramString = paramString.Remove(paramString.LastIndexOf(','), 1);
+                keyString = keyString.Remove(keyString.LastIndexOf(','), 1);
+
+                MySQLCommand.CommandText = $"INSERT INTO {tableAttribute.Name} ({keyString}) VALUES ({paramString})";
+                MySQLCommand.ExecuteNonQuery();
+            }
+            catch (Exception ex)
             {
-                Console.WriteLine($"FILL FIELD DEBUG: @{field.Name.Normalize()}, Value: {field.GetValue(obj)}");
-                command.Parameters.AddWithValue($"@{field.Name.Normalize()}", field.GetValue(obj));
+                LoggerHelper.LogException("Error in TLibrary:");
+                LoggerHelper.LogError(ex);
             }
         }
 
-        public static void GenerateUpdateCommand<T>(this MySqlCommand command, string tablename, T value)
+        public static T GetTableRow<T>(this MySqlConnection connection)
         {
-            string paramString = string.Empty;
-            string conditionString = string.Empty;
+            try
+            {
+                var schemaType = typeof(T);
+                var tableAttribute = schemaType.GetCustomAttribute<SqlNameAttribute>();
+                if (tableAttribute == null)
+                    throw new ArgumentNullException("The given schemaObj does not have SqlNameAttribute.");
 
-            command.CommandText = $"UPDATE {tablename} SET {paramString} WHERE {conditionString}";
+                MySqlCommand MySQLCommand = connection.CreateCommand();
+
+                MySQLCommand.CommandText = $"SELECT * FROM {tableAttribute.Name} WHERE itemID=@ID;";
+                MySqlDataReader Reader = MySQLCommand.ExecuteReader();
+
+                if (Reader == null)
+                    return default;
+
+                T val = default;
+                while (Reader.Read())
+                {
+                    val = Reader.ConvertToObject<T>();
+                    break;
+                }
+                Reader.Close();
+                MySQLCommand.ExecuteNonQuery();
+                return val;
+            }
+            catch (Exception ex)
+            {
+                LoggerHelper.LogException("Error in TLibrary:");
+                LoggerHelper.LogError(ex);
+                return default;
+            }
         }
     }
 }
