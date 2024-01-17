26148786ce84f1d1d58ccd4e47f84de72b03b7ae
Tavstal <tavstal@gmail.com>
2024-01-17 16:27:16 +0100

    AssemblyInfo changes

--- TLibrary/Properties/AssemblyInfo.cs
+++ TLibrary/Properties/AssemblyInfo.cs
@@ -8,9 +8,9 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyTitle("TLibrary")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("Tavstal Plugins")]
+[assembly: AssemblyCompany("Tavstal")]
 [assembly: AssemblyProduct("TLibrary")]
-[assembly: AssemblyCopyright("Copyright © Tavstal 2023")]
+[assembly: AssemblyCopyright("Copyright © 2024 Tavstal")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("en")]
 
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.001.*")]
+[assembly: AssemblyVersion("1.0.*")]
 //[assembly: AssemblyFileVersion("1.0.0.0")]
