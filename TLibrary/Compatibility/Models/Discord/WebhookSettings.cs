071fa008e6ea917943cbabcc745a6e7bfc063b28
Tavstal <tavstal@gmail.com>
2024-01-25 08:03:10 +0100

    Added webhook settings

---
+++ TLibrary/Compatibility/Models/Discord/WebhookSettings.cs
@@ -0,0 +1,32 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tavstal.TLibrary.Compatibility.Models.Discord
+{
+    [Serializable]
+    public class WebhookSettings
+    {
+        [JsonProperty("WebhookUrl")]
+        public string WebhookUrl { get; set; }
+        [JsonProperty("Name")]
+        public string Name { get; set; }
+        [JsonProperty("AvatarUrl")]
+        public string AvatarUrl { get; set; }
+        [JsonProperty("Color")]
+        public string Color { get; set; }
+
+        public WebhookSettings() { }
+
+        public WebhookSettings(string webhookUrl, string name = null, string avatarUrl = null, string color = null)
+        {
+            WebhookUrl = webhookUrl;
+            Name = name;
+            AvatarUrl = avatarUrl;
+            Color = color;
+        }
+    }
+}
