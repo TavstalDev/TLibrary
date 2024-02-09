13fbe0d3e3f6200c660fb9db188422bbd383278f
Tavstal <tavstal@gmail.com>
2024-02-09 08:32:39 +0100

    Added systemExtensions

---
+++ TLibrary/Extensions/SystemExtensions.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Reflection;
+
+namespace Tavstal.TLibrary.Extensions
+{
+    public static class SystemExtensions
+    {
+        public static MethodInfo GetMethod(this Type self, string name, BindingFlags flags, Type[] types)
+        {
+            return self.GetMethod(name, flags, null, types, null);
+        }
+    }
+}
