ff084df6614e9ce4cf0f227814b0d0f897ee2431
TavstalDev <tavstal@gmail.com>
2024-07-18 15:25:02 +0200

    Fix attempt

--- TLibrary/Helpers/Unturned/UAssetHelper.cs
+++ TLibrary/Helpers/Unturned/UAssetHelper.cs
@@ -87,6 +87,17 @@ namespace Tavstal.TLibrary.Helpers.Unturned
         /// </summary>
         /// <param name="id">The ID of the vehicle asset to find.</param>
         /// <returns>A VehicleAsset object representing the vehicle asset with the specified ID, or null if not found.</returns>
-        public static VehicleAsset FindVehicleAsset(ushort id) => Assets.FindVehicleAssetByGuidOrLegacyId(Guid.Empty, id);
+        public static VehicleAsset FindVehicleAsset(ushort id) {
+            VehicleAsset asset = null;
+
+            foreach (VehicleAsset a in GetVehicleAssets()) {
+                if (a.id == id) {
+                    asset = a;
+                    break;
+                }
+            }
+
+            return asset;
+        }
     }
 }
