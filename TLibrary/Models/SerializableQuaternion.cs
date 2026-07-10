3ab27a5afa79f243fd1fcbac09e82bd004a0a7dd
tavstal <tavstal@gmail.com>
2026-07-10 22:15:01 +0200

    Enhance SerializableQuaternion and SerializableVector3 with XML documentation

--- TLibrary/Models/SerializableQuaternion.cs
+++ TLibrary/Models/SerializableQuaternion.cs
@@ -3,16 +3,41 @@ using UnityEngine;
 
 namespace Tavstal.TLibrary.Models
 {
+    /// <summary>
+    /// Quaternion that can be serialized.
+    /// </summary>
     [Serializable]
     public class SerializableQuaternion
     {
+        /// <summary>
+        /// The X component of the quaternion.
+        /// </summary>
         public float X { get; set; }
+        /// <summary>
+        /// The Y component of the quaternion.
+        /// </summary>
         public float Y { get; set; }
+        /// <summary>
+        /// The Z component of the quaternion.
+        /// </summary>
         public float Z { get; set; }
+        /// <summary>
+        /// The W component of the quaternion.
+        /// </summary>
         public float W { get; set; }
 
+        /// <summary>
+        /// Creates a new quaternion with all values set to 0.
+        /// </summary>
         public SerializableQuaternion() { }
 
+        /// <summary>
+        /// Creates a new quaternion with the specified values.
+        /// </summary>
+        /// <param name="x">The X component.</param>
+        /// <param name="y">The Y component.</param>
+        /// <param name="z">The Z component.</param>
+        /// <param name="w">The W component.</param>
         public SerializableQuaternion(float x, float y, float z, float w)
         {
             X = x;
@@ -21,6 +46,10 @@ namespace Tavstal.TLibrary.Models
             W = w;
         }
 
+        /// <summary>
+        /// Creates a new quaternion from a Unity <see cref="Quaternion"/>.
+        /// </summary>
+        /// <param name="quaternion">The Unity quaternion to copy values from.</param>
         public SerializableQuaternion(Quaternion quaternion)
         {
             X = quaternion.x;
@@ -29,6 +58,10 @@ namespace Tavstal.TLibrary.Models
             W = quaternion.w;
         }
         
+        /// <summary>
+        /// Creates a new quaternion by parsing a string in the format "X;Y;Z;W".
+        /// </summary>
+        /// <param name="rawValue">The string to parse (e.g. "0;0;0;1").</param>
         public SerializableQuaternion(string rawValue) {
             string[] parts = rawValue.Split(';');
             X = float.Parse(parts[0]);
@@ -37,7 +70,12 @@ namespace Tavstal.TLibrary.Models
             W = float.Parse(parts[3]);
         }
 
-        public override bool Equals(object obj)
+        /// <summary>
+        /// Checks if this quaternion equals another object.
+        /// </summary>
+        /// <param name="obj">The object to compare with.</param>
+        /// <returns><see langword="true"/> if the values are approximately equal.</returns>
+        public override bool Equals(object? obj)
         {
             if (obj is SerializableQuaternion other && Mathf.Approximately(X, other.X) && Mathf.Approximately(Y, other.Y) && Mathf.Approximately(Z, other.Z) && Mathf.Approximately(W, other.W))
                 return true;
@@ -45,19 +83,36 @@ namespace Tavstal.TLibrary.Models
             return obj is Quaternion otherQ && Mathf.Approximately(X, otherQ.x) && Mathf.Approximately(Y, otherQ.y) && Mathf.Approximately(Z, otherQ.z) && Mathf.Approximately(W, otherQ.w);
         }
 
+        /// <summary>
+        /// Converts this serializable quaternion to a Unity <see cref="Quaternion"/>.
+        /// </summary>
+        /// <returns>A Unity <see cref="Quaternion"/> with the same values.</returns>
         public Quaternion GetQuaternion() => new Quaternion(X, Y, Z, W);
 
+        /// <summary>
+        /// Returns the quaternion as a string in "X;Y;Z;W" format.
+        /// </summary>
+        /// <returns>A string like "0;0;0;1".</returns>
         public override string ToString()
         {
             return $"{X};{Y};{Z};{W}";
         }
 
+        /// <summary>
+        /// Returns the hash code for this quaternion.
+        /// </summary>
+        /// <returns>An integer hash code.</returns>
         public override int GetHashCode()
         {
             // ReSharper disable once BaseObjectGetHashCodeCallInGetHashCode
             return base.GetHashCode();
         }
         
+        /// <summary>
+        /// Converts a Unity <see cref="Quaternion"/> to its string representation.
+        /// </summary>
+        /// <param name="q">The Unity quaternion to serialize.</param>
+        /// <returns>A string in "X;Y;Z;W" format.</returns>
         public static string Serialize(Quaternion q)
         {
             return $"{q.x};{q.y};{q.z};{q.w}";
--- TLibrary/Models/SerializableVector3.cs
+++ TLibrary/Models/SerializableVector3.cs
@@ -9,12 +9,28 @@ namespace Tavstal.TLibrary.Models
     [Serializable]
     public class SerializableVector3
     {
+        /// <summary>
+        /// The X coordinate of the vector.
+        /// </summary>
         public float X { get; set; }
+        /// <summary>
+        /// The Y coordinate of the vector.
+        /// </summary>
         public float Y { get; set; }
+        /// <summary>
+        /// The Z coordinate of the vector.
+        /// </summary>
         public float Z { get; set; }
 
+        /// <summary>
+        /// Creates a new vector with all values set to 0.
+        /// </summary>
         public SerializableVector3() { }
 
+        /// <summary>
+        /// Creates a new vector from a Unity <see cref="Vector3"/>.
+        /// </summary>
+        /// <param name="p">The Unity vector to copy values from.</param>
         public SerializableVector3(Vector3 p)
         {
             X = p.x;
@@ -22,12 +38,22 @@ namespace Tavstal.TLibrary.Models
             Z = p.z;
         }
 
+        /// <summary>
+        /// Creates a new vector with the specified coordinates.
+        /// </summary>
+        /// <param name="x">The X coordinate.</param>
+        /// <param name="y">The Y coordinate.</param>
+        /// <param name="z">The Z coordinate.</param>
         public SerializableVector3(float x, float y, float z) {
             X = x;
             Y = y;
             Z = z;
         }
 
+        /// <summary>
+        /// Creates a new vector by parsing a string in the format "X;Y;Z".
+        /// </summary>
+        /// <param name="rawValue">The string to parse (e.g. "1.5;2.0;3.2").</param>
         public SerializableVector3(string rawValue) {
             string[] parts = rawValue.Split(';');
             X = float.Parse(parts[0]);
@@ -35,7 +61,12 @@ namespace Tavstal.TLibrary.Models
             Z = float.Parse(parts[2]);
         }
 
-        public override bool Equals(object obj)
+        /// <summary>
+        /// Checks if this vector equals another object.
+        /// </summary>
+        /// <param name="obj">The object to compare with.</param>
+        /// <returns><see langword="true"/> if the values are approximately equal.</returns>
+        public override bool Equals(object? obj)
         {
             if (obj is SerializableVector3 other && Mathf.Approximately(X, other.X) && Mathf.Approximately(Y, other.Y) && Mathf.Approximately(Z, other.Z))
                 return true;
@@ -43,19 +74,36 @@ namespace Tavstal.TLibrary.Models
             return obj is Vector3 other3 && Mathf.Approximately(X, other3.x) && Mathf.Approximately(Y, other3.y) && Mathf.Approximately(Z, other3.z);
         }
 
+        /// <summary>
+        /// Converts this serializable vector to a Unity <see cref="Vector3"/>.
+        /// </summary>
+        /// <returns>A Unity <see cref="Vector3"/> with the same coordinates.</returns>
         public Vector3 GetVector3() => new Vector3(X, Y, Z);
 
+        /// <summary>
+        /// Returns the vector as a string in "X;Y;Z" format.
+        /// </summary>
+        /// <returns>A string like "1.5;2.0;3.2".</returns>
         public override string ToString()
         {
             return $"{X};{Y};{Z}";
         }
 
+        /// <summary>
+        /// Returns the hash code for this vector.
+        /// </summary>
+        /// <returns>An integer hash code.</returns>
         public override int GetHashCode()
         {
             // ReSharper disable once BaseObjectGetHashCodeCallInGetHashCode
             return base.GetHashCode();
         }
         
+        /// <summary>
+        /// Converts a Unity <see cref="Vector3"/> to its string representation.
+        /// </summary>
+        /// <param name="v">The Unity vector to serialize.</param>
+        /// <returns>A string in "X;Y;Z" format.</returns>
         public static string Serialize(Vector3 v)
         {
             return $"{v.x};{v.y};{v.z}";
