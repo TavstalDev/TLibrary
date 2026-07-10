75fb69f8755dbc26472208c7d450b892ee3d2354
tavstal <tavstal@gmail.com>
2026-07-10 23:02:55 +0200

    Refactor ConfigurationBase class to enhance logging configuration and improve documentation

--- TLibrary/Models/Plugin/ConfigurationBase.cs
+++ TLibrary/Models/Plugin/ConfigurationBase.cs
@@ -13,15 +13,15 @@ namespace Tavstal.TLibrary.Models.Plugin
         [JsonIgnore]
         public string FileName { get; set; }
         [JsonProperty(Order = 0)]
-        public bool DebugMode { get; set; }
-        [JsonProperty(Order = 1)]
-        public string Locale { get; set; }
+        public ELogLevel LogLevel { get; set; }
+        [JsonProperty(Order = 1)] 
+        public string Locale { get; set; } = "en";
         [JsonProperty(Order = 2)]
         public bool DownloadLocalePacks { get; set; }
 
         /// <param name="filename">Example: myfile.txt</param>
         /// <param name="path">Example: D:\MyDirectory</param>
-        public ConfigurationBase(string filename, string path)
+        protected ConfigurationBase(string filename, string path)
         {
             FilePath = path;
             FileName = filename;
@@ -29,24 +29,37 @@ namespace Tavstal.TLibrary.Models.Plugin
             LoadDefaults();
         }
 
-        public ConfigurationBase()
+        /// <summary>
+        /// Creates a configuration base with default empty values.
+        /// </summary>
+        protected ConfigurationBase()
         {
-
+            FilePath = string.Empty;
+            FileName = string.Empty;
         }
 
-        public virtual void LoadDefaults()
-        {
+        /// <summary>
+        /// Called when the configuration is created to set default values.
+        /// Override this to define custom defaults for your configuration.
+        /// </summary>
+        public virtual void LoadDefaults() { }
 
-        }
+        /// <summary>
+        /// Creates a new instance of type <typeparamref name="T"/> using the parameterless constructor.
+        /// </summary>
+        /// <typeparam name="T">The type of configuration to create.</typeparam>
+        /// <returns>A new instance of <typeparamref name="T"/>.</returns>
+        public static T Create<T>() where T : ConfigurationBase => 
+            Activator.CreateInstance<T>(); 
 
-        public static T Create<T>() where T : ConfigurationBase
-        {
-            return Activator.CreateInstance<T>(); 
-        }
-
-        public static T Create<T>(string fileName, string path) where T : ConfigurationBase
-        {
-            return (T)Activator.CreateInstance(typeof(T), fileName, path);
-        }
+        /// <summary>
+        /// Creates a new instance of type <typeparamref name="T"/> with the given file name and path.
+        /// </summary>
+        /// <param name="fileName">The name of the configuration file.</param>
+        /// <param name="path">The directory path of the configuration file.</param>
+        /// <typeparam name="T">The type of configuration to create.</typeparam>
+        /// <returns>A new instance of <typeparamref name="T"/>.</returns>
+        public static T Create<T>(string fileName, string path) where T : ConfigurationBase =>
+            (T)Activator.CreateInstance(typeof(T), fileName, path);
     }
 }
