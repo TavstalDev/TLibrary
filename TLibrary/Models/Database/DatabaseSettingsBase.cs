0e82dd9a2d340fb147137285ab5316fa885a73cd
tavstal <tavstal@gmail.com>
2026-07-10 22:14:33 +0200

    Enhance DatabaseSettingsBase and IDatabaseSettings with XML documentation

--- TLibrary/Models/Database/DatabaseSettingsBase.cs
+++ TLibrary/Models/Database/DatabaseSettingsBase.cs
@@ -2,18 +2,30 @@
 
 namespace Tavstal.TLibrary.Models.Database
 {
+    /// <inheritdoc/>
     public abstract class DatabaseSettingsBase : IDatabaseSettings
     {
+        /// <inheritdoc/>
         [JsonProperty(Order = 0)]
         public string Host { get; set; } = "127.0.0.1";
+        
+        /// <inheritdoc/>
         [JsonProperty(Order = 1)]
         public int Port { get; set; } = 3306;
+        
+        /// <inheritdoc/>
         [JsonProperty(Order = 2)]
         public string DatabaseName { get; set; } = "unturned";
+        
+        /// <inheritdoc/>
         [JsonProperty(Order = 3)]
         public string UserName { get; set; } = "root";
+        
+        /// <inheritdoc/>
         [JsonProperty(Order = 4)]
         public string UserPassword { get; set; } = "ascent";
+        
+        /// <inheritdoc/>
         [JsonProperty(Order = 5)]
         public int TimeOut { get; set; } = 120;
     }
--- TLibrary/Models/Database/IDatabaseSettings.cs
+++ TLibrary/Models/Database/IDatabaseSettings.cs
@@ -9,22 +9,27 @@
         /// Address of the server
         /// </summary>
         string Host { get; set; }
+        
         /// <summary>
         /// Port of the server
         /// </summary>
         int Port { get; set; }
+        
         /// <summary>
         /// Name of the database
         /// </summary>
         string DatabaseName { get; set; }
+        
         /// <summary>
         /// Username of the database account
         /// </summary>
         string UserName { get; set; }
+        
         /// <summary>
         /// Password of the database account
         /// </summary>
         string UserPassword { get; set; }
+        
         /// <summary>
         /// Timeout of the connection in seconds
         /// <br/>how long the connection should wait for a response from the server before disconnecting
