8b0cd724f8963b2451975776de4234bd5886f344
tavstal <tavstal@gmail.com>
2026-07-10 23:01:05 +0200

    Improve documentation in PlayerExtensions for clarity and consistency

--- TLibrary/Extensions/Unturned/PlayerExtensions.cs
+++ TLibrary/Extensions/Unturned/PlayerExtensions.cs
@@ -8,34 +8,37 @@ using UnityEngine;
 
 namespace Tavstal.TLibrary.Extensions.Unturned
 {
+    /// <summary>
+    /// Provides extension methods for players.
+    /// </summary>
     public static class PlayerExtensions
     {
         /// <summary>
-        /// Checks if the Steam user associated with the specified Steam ID is currently online.
+        /// Checks if the user with this Steam ID is currently online.
         /// </summary>
-        /// <param name="id">The Steam ID of the user to check.</param>
-        /// <returns>True if the user is online, false otherwise.</returns>
+        /// <param name="id">The Steam ID to check.</param>
+        /// <returns>True if the user is online, false if not.</returns>
         public static bool isOnline(this CSteamID id)
         {
             return Provider.clients.Any(x => x.playerID.steamID == id);
         }
 
         /// <summary>
-        /// Checks if the Unturned player is currently online.
+        /// Checks if the player is currently online.
         /// </summary>
-        /// <param name="player">The Unturned player to check.</param>
-        /// <returns>True if the player is online, false otherwise.</returns>
+        /// <param name="player">The player to check.</param>
+        /// <returns>True if the player is online, false if not.</returns>
         public static bool isOnline(this UnturnedPlayer player)
         {
             return isOnline(player.CSteamID);
         }
 
         /// <summary>
-        /// Gets a list of Unturned players who are nearby a given position within a specified distance.
+        /// Finds all players that are close to this player within a given distance.
         /// </summary>
-        /// <param name="uplayer">The position of the player to check for nearby players.</param>
-        /// <param name="distance">The maximum distance within which players are considered nearby.</param>
-        /// <returns>A list of Unturned players who are nearby the given position.</returns>
+        /// <param name="uplayer">The player to check around.</param>
+        /// <param name="distance">How far to look for other players.</param>
+        /// <returns>A list of nearby players.</returns>
         public static List<UnturnedPlayer> GetNearbyPlayers(this UnturnedPlayer uplayer, float distance)
         {
             List<UnturnedPlayer> players = new List<UnturnedPlayer>();
@@ -50,35 +53,35 @@ namespace Tavstal.TLibrary.Extensions.Unturned
         }
 
         /// <summary>
-        /// Searches for inventory items of the specified type in the player's inventory.
+        /// Looks for items of the given type in the player's inventory.
         /// </summary>
-        /// <param name="inventory">The PlayerInventory to search.</param>
-        /// <param name="type">The EItemType representing the type of items to search for.</param>
-        /// <param name="findEmpty">Flag indicating whether to include empty amount in the search. (Default is false)</param>
-        /// <returns>A List of InventorySearch containing the search results.</returns>
+        /// <param name="inventory">The inventory to search.</param>
+        /// <param name="type">The item type to look for.</param>
+        /// <param name="findEmpty">If true, also includes items with empty amount. Default is false.</param>
+        /// <returns>A list of search results.</returns>
         public static List<InventorySearch> Search(this PlayerInventory inventory, EItemType type, bool findEmpty = false) => UPlayerHelper.Search(inventory, type, findEmpty);
 
         /// <summary>
-        /// Searches for inventory items with the specified item ID in the player's inventory.
+        /// Looks for items with the given ID in the player's inventory.
         /// </summary>
-        /// <param name="inventory">The PlayerInventory to search.</param>
-        /// <param name="itemID">The item ID to search for.</param>
-        /// <param name="findEmpty">Flag indicating whether to include empty amount in the search. (Default is false)</param>
-        /// <returns>A List of InventorySearch containing the search results.</returns>
+        /// <param name="inventory">The inventory to search.</param>
+        /// <param name="itemID">The item ID to look for.</param>
+        /// <param name="findEmpty">If true, also includes items with empty amount. Default is false.</param>
+        /// <returns>A list of search results.</returns>
         public static List<InventorySearch> Search(this PlayerInventory inventory, ushort itemID, bool findEmpty = false) => UPlayerHelper.Search(inventory, itemID, findEmpty);
 
         /// <summary>
-        /// Forces the player to teleport to the specified location within a given radius.
+        /// Teleports the player to a location within a given radius.
         /// </summary>
         /// <param name="player">The player to teleport.</param>
-        /// <param name="location">The destination location for the teleportation.</param>
-        /// <param name="radious">The maximum distance from the destination location where the player can teleport. (Default is 3.0f)</param>
+        /// <param name="location">Where to teleport the player.</param>
+        /// <param name="radious">The maximum distance from the target location. Default is 3.</param>
         public static void ForceTeleport(this Player player, Vector3 location, float radious = 3f) => UPlayerHelper.ForceTeleport(player, location, radious);
 
         /// <summary>
-        /// Destroys all barricades belonging to the specified player.
+        /// Destroys all barricades owned by the player.
         /// </summary>
-        /// <param name="player">The player whose barricades will be destroyed.</param>
+        /// <param name="player">The player whose barricades to destroy.</param>
         public static void DestroyPlayerBarricades(this UnturnedPlayer player) => UPlayerHelper.DestroyPlayerBarricades(player);
     }
 }
