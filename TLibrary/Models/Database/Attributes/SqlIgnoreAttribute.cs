bb3d0a2933f8875d44c03dad2441eba04c6d5ca0
tavstal <tavstal@gmail.com>
2026-07-10 22:13:32 +0200

    Refactor SqlIgnoreAttribute and SqlMemberAttribute for improved clarity and nullability

--- TLibrary/Models/Database/Attributes/SqlIgnoreAttribute.cs
+++ TLibrary/Models/Database/Attributes/SqlIgnoreAttribute.cs
@@ -5,9 +5,7 @@ namespace Tavstal.TLibrary.Models.Database.Attributes
     /// <summary>
     /// SQL Attribute used to ignore variables by the database manager
     /// </summary>
-    [AttributeUsage(AttributeTargets.Property | AttributeTargets.Field, AllowMultiple = true, Inherited = false)]
+    [AttributeUsage(AttributeTargets.Property | AttributeTargets.Field, AllowMultiple = true)]
     // ReSharper disable once ClassNeverInstantiated.Global
-    public class SqlIgnoreAttribute : Attribute
-    {
-    }
+    public class SqlIgnoreAttribute : Attribute { }
 }
